44e53974d3fc41fa5a326e8e566c58ef199603fd
Your Name <you@example.com>
2024-06-06 23:27:18 +0300

    23

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -11,13 +11,13 @@ import org.junit.runner.RunWith;
                 "html:target/reports/html-reports/default-cucumber-reports.html",//html formatında rapor için
                 "json:target/reports/json-reports/cucumber1.json",//json formatında rapor için
                 "junit:target/reports/xml-reports/cucumber1.xml",//xml formatında rapor için
-                "rerun:target/failedScenarios.txt",//Fail olan testleri bu text dosyasına yazar ve tekara çalıştırılmalarına yardımcı olur.
+                "rerun:target/failedScenarios.txt",//Fail olan testleri bu text dosyasına yazar ve tekrar çalıştırılmalarına yardımcı olur.
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",//Spark Report için
                 "pretty"//Konsola daha güzel renkli yazdırma yapar.
         },
         features = "src/test/resources/features",
         glue = "stepdefinitions",
-        tags = "@Excel",
+        tags = "@XYZBank",
         dryRun = false//'dryRun = true' testi çalıştırmaz, eksik step deinitionları bulur.
 )
 
